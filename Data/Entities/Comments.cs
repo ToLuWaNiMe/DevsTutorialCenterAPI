2d5e3c05f1cf886eec0b8cb6d3ca395b00c25f7a
Aderoju99 <abiolaaderoju9@gmail.com>
2023-09-12 16:00:57 +0100

    remove guid

--- Data/Entities/Comments.cs
+++ Data/Entities/Comments.cs
@@ -3,7 +3,6 @@ namespace DevsTutorialCenterAPI.Data.Entities
 {
 	public class Comments
 	{
-		public Guid Id { get; set; }
 		public string Text { get; set; }
         public string UserId { get; set; }
     }
