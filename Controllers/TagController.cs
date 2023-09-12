0f5b4ec2ce78802b096c04534ea8bd403fe045da
pecomen <ortizpeace@gmail.com>
2023-09-12 21:33:10 +0100

    third Commit

--- Controllers/TagController.cs
+++ Controllers/TagController.cs
@@ -20,30 +20,20 @@ namespace DevsTutorialCenterAPI.Controllers
 
 
         [HttpGet]
-        public async Task<ActionResult<ResponseDto<IEnumerable<Tag>>>> GetAllTagAsync(int page = 1, int pageSize = 10)
+        public async Task<ActionResult<ResponseDto<IEnumerable<Tag>>>> GetAllTagAsync()
         {
             var tags = await _tagService.GetAllTagAsync();
-
-            // Calculate the total number of items and pages
-            int totalItems = tags.Count();
-
-
-            // Calculate the number of items to skip and take for the current page
-            int skip = (page - 1) * pageSize;
-            int take = pageSize;
-
-            // Retrieve the paged tags
-            var pagedTags = tags.Skip(skip).Take(take).ToList();
 
             return Ok(new ResponseDto<IEnumerable<Tag>>
             {
                 Code = 200,
                 Message = "OK",
                 Error = "",
-                Data = pagedTags,
-
-
             });
-        }
+
+
+         }
     }
 }
+            
+
