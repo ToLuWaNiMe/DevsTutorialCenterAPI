0f5fbabe7a316eae3aa716dc5111d4811aa8e169
Aderoju99 <abiolaaderoju9@gmail.com>
2023-09-13 14:33:13 +0100

    update

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,7 +1,9 @@
-﻿using DevsTutorialCenterAPI.Services.Abstractions;
+﻿using DevsTutorialCenterAPI.Models.DTOs;
+using DevsTutorialCenterAPI.Services.Abstractions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace DevsTutorialCenterAPI.Controllers
 {
@@ -20,8 +22,42 @@ namespace DevsTutorialCenterAPI.Controllers
         
         public async Task<IActionResult> GetCommentsByArticle(string ArticleId)
         {
-            var comments = await _commentService.GetCommentsByArticle(ArticleId);
+           
+            var response = new ResponseDto<object>();
 
+            try
+            {
+                var comments = await _commentService.GetCommentsByArticle(ArticleId);
+
+                var noOfComments = comments.Count();
+
+                if (!comments.Any())
+                {
+                    response.Code = (int)HttpStatusCode.NoContent;
+                    response.Message = $"no comments found for {ArticleId}";
+                    response.Data = null;
+                    response.Error = "";
+
+                    return BadRequest(response);
+                }
+
+                response.Code = (int)HttpStatusCode.OK;
+                response.Message = $"{noOfComments} Comments found";
+                response.Data = comments;
+                response.Error = "";
+            }
+            catch (Exception ex)
+            {
+                response.Code = (int)HttpStatusCode.InternalServerError;
+                response.Message = "Error";
+                response.Data = null;
+                response.Error = ex.Message;
+
+                return BadRequest(response);
+            }
+
+            return Ok(response);
         }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddDbContext<DevsTutorialCenterAPIContext>(
 );
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddScoped<IArticleService, ArticleService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
 
 
 
