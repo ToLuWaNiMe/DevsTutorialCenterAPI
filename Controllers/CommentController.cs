be8c59112e52e13ef21f5ab1082bee049f0836c0
Aderoju99 <abiolaaderoju9@gmail.com>
2023-09-13 13:24:03 +0100

    updated

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -10,51 +10,52 @@ namespace DevsTutorialCenterAPI.Controllers
     [Authorize]
     [Route("api/comments")]
     [ApiController]
-         public class CommentsController : ControllerBase
-        {
-            private readonly ICommentService _commentService;
-            public CommentsController(ICommentService commentService)
-            {
-                _commentService = commentService;
-            }
-            // DELETE endpoint for deleting a comment
-            [AllowAnonymous]
-            [HttpDelete("{commentId}")]
-            public async Task<IActionResult> DeleteComment(string commentId)
-            {
-                var response = new ResponseDto<object>();
+    public class CommentsController : ControllerBase
+    {
+       private readonly ICommentService _commentService;
+       public CommentsController(ICommentService commentService)
+       {
+           _commentService = commentService;
+       }
+       // DELETE endpoint for deleting a comment
+       [Authorize]
+       [HttpDelete("{commentId}")]
+       public async Task<IActionResult> DeleteComment(string commentId)
+       {
+           var response = new ResponseDto<object>();
 
-                try
-                {
-                    var isDeleted = await _commentService.DeleteCommentAsync(commentId);
-                    if (!isDeleted)
-                    {
-                        response.Code = 400;
-                        response.Message = "Comment not deleted";
-                        response.Data = null;
-                        response.Error = "";
+           try
+           {
+               var isDeleted = await _commentService.DeleteCommentAsync(commentId);
+               if (!isDeleted)
+               {
+                   response.Code = 400;
+                   response.Message = "Comment not deleted";
+                   response.Data = null;
+                   response.Error = "";
 
-                        return BadRequest(response);
-                    }
+                   return BadRequest(response);
+               }
 
-                    response.Code = 200;
-                    response.Message = "deleted successfully";
-                    response.Data = "";
-                    response.Error = "";
-                }
-                catch (Exception ex)
-                {
-                    response.Code = 500;
-                    response.Message = "Error";
-                    response.Data = null;
-                    response.Error = ex.Message;
+               response.Code = 200;
+               response.Message = "deleted successfully";
+               response.Data = "";
+               response.Error = "";
+           }
+           catch (Exception ex)
+           {
+               response.Code = 500;
+               response.Message = "Error";
+               response.Data = null;
+               response.Error = ex.Message;
 
-                    return BadRequest(response);
-                }
+               return BadRequest(response);
+           }
 
-                return Ok(response);
-            }
-        }
+           return Ok(response);
+       }
 
     }
+
+}
 
