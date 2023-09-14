0d3842125a1d9cbf60445349a9ac85babfeae5f5
La0lu <oguntoyetobi456@gmail.com>
2023-09-14 08:28:07 +0100

    updated

--- Services/Implementations/ArticleService.cs
+++ Services/Implementations/ArticleService.cs
@@ -48,11 +48,5 @@ namespace DevsTutorialCenterAPI.Services.Implementations
             await _repository.DeleteAsync(article);
             return true;
         }
-
-        //public async Task<Article> GetArticleById(string articleId)
-        //{
-        //    var article = await _repository.GetByIdAsync<Article>(articleId);
-        //    return article;
-        //}
     }
 }
