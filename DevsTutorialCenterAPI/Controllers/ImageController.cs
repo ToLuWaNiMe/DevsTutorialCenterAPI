ce409ce015c2ad00ca46ee49503a63395009f046
covenantebube <covenantebube1@gmail.com>
2023-10-30 12:57:32 +0100

    save

--- DevsTutorialCenterAPI/Controllers/ImageController.cs
+++ DevsTutorialCenterAPI/Controllers/ImageController.cs
@@ -3,98 +3,115 @@ using DevsTutorialCenterAPI.Services.Abstractions;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace DevsTutorialCenterAPI.Controllers
+namespace DevsTutorialCenterAPI.Controllers;
+
+// [Authorize]
+[ApiController]
+[Route("api/images")]
+[Authorize]
+public class ImageController : ControllerBase
 {
-    [ApiController]
-    [Route("api/images")]
-    [Authorize]
-    public class ImageController : ControllerBase
+    private readonly IArticleService _articleService;
+    private readonly IImageService _imageService;
+
+
+    public ImageController(IImageService imageService, IArticleService articleService)
     {
-        private readonly IImageService _imageService;
+        _imageService = imageService;
+        _articleService = articleService;
+    }
 
-        public ImageController(IImageService imageService)
+    [HttpPost]
+    public async Task<IActionResult> UploadImage([FromForm] ImageUploadRequestDto requestDto)
+    {
+        try
         {
-            _imageService = imageService;
-        }
+            var article = await _articleService.GetArticleById(requestDto.ArticleId);
 
-        [HttpPost("upload")]
-        public async Task<IActionResult> UploadImage([FromForm] ImageUploadRequestDto requestDto)
-        {
-            try
-            {
-                var uploadResult = await _imageService.AddImageAsync(requestDto.Photo);
 
-                if (uploadResult.Error != null)
+            if (article == null)
+                return StatusCode(403, new ResponseDto<object>
                 {
-                    return BadRequest(new ResponseDto<object>
-                    {
-                        Code = 400,
-                        Message = "Error",
-                        Data = null,
-                        Error = uploadResult.Error.Message
-                    });
-                }
+                    Code = 403,
+                    Message = "Error",
+                    Data = null,
+                    Error = "No article was found with the given id."
+                });
 
+            var uploadResult = await _imageService.AddImageAsync(requestDto.Photo);
+
+            if (uploadResult.Error != null)
+                return BadRequest(new ResponseDto<object>
+                {
+                    Code = 400,
+                    Message = "Error",
+                    Data = null,
+                    Error = uploadResult.Error.Message
+                });
+
+
+            article.PublicId = uploadResult.PublicId;
+            article.ImageUrl = uploadResult.SecureUrl.AbsoluteUri;
+
+            await _articleService.UpdateArticleAsync(article);
+
+            return Ok(new ResponseDto<object>
+            {
+                Code = 200,
+                Message = "Ok",
+                Data = new
+                {
+                    uploadResult.PublicId,
+                    Url = uploadResult.SecureUrl.AbsoluteUri
+                },
+                Error = ""
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new ResponseDto<object>
+            {
+                Code = 400,
+                Message = "Error",
+                Data = null,
+                Error = "Failed to upload image: " + ex.Message
+            });
+        }
+    }
+
+
+    [HttpDelete("{publicId}")]
+    public async Task<IActionResult> DeleteImage(string publicId)
+    {
+        try
+        {
+            var deletionResult = await _imageService.DeleteImageAsync(publicId);
+
+            if (deletionResult.Result == "ok")
                 return Ok(new ResponseDto<object>
                 {
                     Code = 200,
                     Message = "Ok",
-                    Data = new
-                    {
-                        PublicId = uploadResult.PublicId,
-                        Url = uploadResult.SecureUri.AbsoluteUri
-                    },
+                    Data = "",
                     Error = ""
                 });
-            }
-            catch (Exception ex)
+            return BadRequest(new ResponseDto<object>
             {
-                return BadRequest(new ResponseDto<object>
-                {
-                    Code = 400,
-                    Message = "Error",
-                    Data = null,
-                    Error = "Failed to upload image: " + ex.Message
-                });
-            }
+                Code = 400,
+                Message = "Error",
+                Data = null,
+                Error = "Failed to delete image from Cloudinary."
+            });
         }
-
-        [HttpDelete("{publicId}")]
-        public async Task<IActionResult> DeleteImage(string publicId)
+        catch (Exception ex)
         {
-            try
+            return BadRequest(new ResponseDto<object>
             {
-                var deletionResult = await _imageService.DeleteImageAsync(publicId);
-
-                if (deletionResult.Result == "ok")
-                {
-                    return Ok(new ResponseDto<object>
-                    {
-                        Code = 200,
-                        Message = "Ok",
-                        Data = "",
-                        Error = ""
-                    });
-                }
-
-                return BadRequest(new ResponseDto<object>
-                {
-                    Code = 400,
-                    Message = "Error",
-                    Data = null,
-                    Error = "Failed to delete image from Cloudinary."
-                });
-            }
-            catch (Exception ex)
-            {
-                return BadRequest(new ResponseDto<object>
-                {
-                    Code = 400,
-                    Message = "Error",
-                    Data = null,
-                    Error = "Failed to delete image: " + ex.Message
-                });
-            }
+                Code = 400,
+                Message = "Error",
+                Data = null,
+                Error = "Failed to delete image: " + ex.Message
+            });
         }
     }
 }
